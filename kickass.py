a267d54765ecd3b62ab0ba293e350cc362b4ff85
Leo Khachatorians <leokhachatorians@gmail.com>
2015-07-30 08:01:29 -0700

    Update kickass.py
    added comments

--- kickass.py
+++ kickass.py
@@ -9,6 +9,15 @@ class Kickass(Torrent):
 		self.lowest_value = 1
 
 	def grab_data(self, soup=None, tree=None):
+		"""
+		Arguments:
+			soup - The beautiful soup object of the page
+			tree - the lxml object of the page
+		Description:
+			Depending on the easiest way to parse for data,
+			uses bs4 or lxml. Has a custom function 'bs4_grabber'
+			for all beautiful objects.
+		"""
 		self.seeds = self.bs4_grabber(soup, class_name='green center')
 		self.leeches = self.bs4_grabber(soup, class_name='red lasttd center')
 		self.titles = self.bs4_grabber(soup, class_name='cellMainLink')
@@ -17,6 +26,17 @@ class Kickass(Torrent):
 		self.website_name = 'Kickass Torrents'
 
 	def start(self):
+		"""
+		Description:
+			1) Loads the page
+			2) Creates the requests objects
+			3) Creates the tree object
+			4) Begins the process of grabbing data
+			
+			Will look to refactor this to make it so
+			each step is its own function, but it does
+			make things more tidy having it in one method.
+		"""
 		url = self.create_complete_url(self.page_num, self.search_term, Kickass=True)
 		r = self.create_requests(url)
 		soup = self.create_soup(r, html_parser=True)
