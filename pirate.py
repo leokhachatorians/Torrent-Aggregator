0bc8525f16dabcae036acd754a9e73a90e2b7c7b
Leo Khachatorians <leokhachatorians@gmail.com>
2015-08-03 12:52:38 -0700

    Update pirate.py
    added comments

--- pirate.py
+++ pirate.py
@@ -10,6 +10,15 @@ class Piratebay(Torrent):
 		self.lowest_value = 0
 
 	def grab_data(self, soup=None, tree=None):
+		"""
+		Arguments:
+			soup - The beautiful soup object of the page
+			tree - the lxml object of the page
+		Description:
+			Depending on the easiest way to parse for data,
+			uses bs4 or lxml. Has a custom function 'bs4_grabber'
+			for all beautiful objects.
+		"""
 		self.titles = self.bs4_grabber(soup, class_name='detLink')
 		self.info = tree.xpath('//*[@id="searchResult"]/tr/td[2]/font/text()')
 
@@ -22,6 +31,17 @@ class Piratebay(Torrent):
 		self.website_name = 'The Pirate Bay'
 
 	def start(self):
+		"""
+		Description:
+			1) Loads the page
+			2) Creates the requests objects
+			3) Creates the tree object
+			4) Begins the process of grabbing data
+			
+			Will look to refactor this to make it so
+			each step is its own function, but it does
+			make things more tidy having it in one method.
+		"""
 		url = self.create_complete_url(self.page_num, self.search_term, Pirate=True)
 		r = self.create_requests(url)
 		soup = self.create_soup(r)
